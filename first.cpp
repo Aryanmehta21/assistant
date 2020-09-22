2e0c854855cb174cc4955a96064b5897cc640f9d
Aryan <aryanmehta310@gmail.com>
2020-09-22 18:08:52 +0530

    final form

---
+++ first.cpp
@@ -0,0 +1,62 @@
+// #include<iostream>
+
+// int main(){
+//     std::cout<<"hello world\n";
+//     std::cout<<"\t thank you!";
+//     return 0;
+// }
+
+
+// #include<iostream>
+
+// using namespace std;
+
+// int main(){
+//     for(int i = 1; i <= 100 ; i++)
+//     {
+//         cout<<i<<endl;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main(){
+//     int i = 1;
+//     while(i<=100){
+//     cout<<i<<endl;
+//     i++;
+//     }
+
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main(){
+//     int(age);
+//     cout<<"Tell me your age"<<endl;
+//     cin>>age;
+//     if((age<18)&&(age>0)){
+//         cout<<"you are a kid";
+//     }
+//     else if(age==18){
+//         cout<<"Next year you will become adult";
+//     }
+//     else{
+//         cout<<"you are adult";
+//     }
+//     return 0;
+// }
+// #include<iostream>
+
+// using namespace std;
+
+// int main(){
+//     for(int i=1;i<=10;i++){
+//         cout<<i*6<<endl;
+//     }
+//     return 0;
+// }
---
+++ if else.py
@@ -0,0 +1,11 @@
+print("enter a no.!")
+a = int(input())
+print("enter a no.!")
+b = int(input())
+
+if a > b:
+    print("hello")
+elif a == b:
+    print("How Are You?")
+else:
+    print("byee")    
